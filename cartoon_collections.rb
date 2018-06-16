ce2fc72864c01a22163d313d9cd0a61894b73926
Christofer Chan <devchumbing@gmail.com>
2018-06-16 20:21:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,12 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|name, index|
+  dwarves.each_with_index { |name, index|
     puts "#{index + 1}. #{name}"
   }
 end
 
 def summon_captain_planet(array)
   array.each.collect { |name|
-    name << !
+    name << "!"
   }
 end
 
